3fabc3f8abf21543c917b0f515fd6d8ae46ed723
serraguti <pacoserranox@gmail.com>
2021-03-18 11:51:27 +0100

    initial commit1

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using ControlCochesCosmosSql.Data;
+using ControlCochesCosmosSql.Repositories;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(ControlCochesCosmosSql.Startup))]
+namespace ControlCochesCosmosSql
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddTransient<RepositoryCoches>();
+            String cadena = Environment.GetEnvironmentVariable("cadenaazure");
+            builder.Services.AddDbContext<CochesContext>(options =>
+            options.UseSqlServer(cadena));
+        }
+    }
+}
