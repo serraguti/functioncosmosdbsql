4ab7b9daada869f20a193c40f260a9ffb57b86f1
serraguti <pacoserranox@gmail.com>
2021-03-18 11:43:05 +0100

    initial commit1

--- Function1.cs
+++ Function1.cs
@@ -1,24 +1,51 @@
 using System;
 using System.Collections.Generic;
+using ControlCochesCosmosSql.Models;
+using ControlCochesCosmosSql.Repositories;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace ControlCochesCosmosSql
 {
-    public static class Function1
+    public class Function1
     {
+        RepositoryCoches repo;
+
+        public Function1(RepositoryCoches repo)
+        {
+            this.repo = repo;
+        }
+
         [FunctionName("functioncosmos")]
-        public static void Run([CosmosDBTrigger(
+        public void Run([CosmosDBTrigger(
             databaseName: "coches",
             collectionName: "micoleccion",
-            ConnectionStringSetting = "cosmosdb")]IReadOnlyList<Document> input, ILogger log)
+            ConnectionStringSetting = "cosmosdb")]
+        IReadOnlyList<Document> input, ILogger log)
         {
             if (input != null && input.Count > 0)
             {
-                log.LogInformation("Documents modified " + input.Count);
-                log.LogInformation("First document Id " + input[0].Id);
+                //LO QUE VIENE SON DATOS JSON
+                var data = JsonConvert.SerializeObject(input);
+                log.LogInformation("Coche cosmos: " + data);
+                List<Coche> coches =
+                    JsonConvert.DeserializeObject<List<Coche>>(data);
+                Coche car = coches[0];
+                if (this.repo.BuscarCoche(car.Id) == null)
+                {
+                    this.repo.InsertarCoche(car.Id, car.Marca, car.Modelo, car.Velocidad);
+                }
+                else
+                {
+                    this.repo.ModificarCoche(car.Id, car.Marca, car.Modelo, car.Velocidad);
+                }
+
+                log.LogInformation("Datos almacenados en SQL!!!");
+                log.LogInformation("Coche SQL: " 
+                    + coches[0].Marca + ", " + coches[0].Modelo);
             }
         }
     }
